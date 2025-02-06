225afa2b023b901340580de82d42b41972b2f762
Ferdinand <144912816+fercananything@users.noreply.github.com>
2025-02-05 23:07:16 -0600

    Change website footer (#6)
    * Adjust height
    
    * Rapihin dikit
    
    * Make good footer
    
    * change logo
    
    * remove karirlab
    
    * Change footer

--- src/navigation.ts
+++ src/navigation.ts
@@ -102,11 +102,10 @@ export const headerData = {
         },
       ],
     },
-    {
-      text: 'Career',
-      href: getPermalink('https://permias.karirlab.co/'),
-      target: '_blank'
-    },
+    // {
+    //   text: 'Career',
+    //   href: getPermalink('https://permias.karirlab.co/')
+    // },
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
@@ -114,63 +113,39 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'SIGN UP FOR OUR NEWSLETTER', href: 'http://eepurl.com/g-RXeT' },
+        { text: 'info@permiasnasional.com', href: 'mailto::info@permiasnasional.com' },
+        { text: '© 2025 PERMIAS NASIONAL', href: '#' }
       ],
     },
     {
-      title: 'Platform',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        { text: 'ABOUT US', href: '/about' },
+        { text: 'CONTACT', href: '/contact' },
+        { text: 'DONATE', href: '/donate' },
       ],
     },
     {
-      title: 'Support',
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'INSTAGRAM', href: 'https://www.instagram.com/permias.nasional/'},
+        { text: 'FACEBOOK', href: 'https://www.facebook.com/permias.nasional' },
+        { text: 'LINKEDIN', href: 'https://www.linkedin.com/company/permiasnasional' },
+        { text: 'YOUTUBE', href: 'https://www.youtube.com/channel/UCHjEGs027y3g--ZH5BCS7tw' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    // { text: 'Terms', href: getPermalink('/terms') },
+    // { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/permias.nasional/' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/permias.nasional' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/permiasnasional' },
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/channel/UCHjEGs027y3g--ZH5BCS7tw' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made by Permias Nasional · All rights reserved.
   `,
 };
