ae1f1709aecfab9e67252e4c10e7e0a2de5f8734
PERMIAS Nasional <medinfo@permiasnasional.com>
2025-02-13 00:45:13 -0500

    Fix extra space in contact.astro and navigation

--- src/navigation.ts
+++ src/navigation.ts
@@ -28,8 +28,29 @@ export const headerData = {
       href: getPermalink('/landing/Essentials')
     },
     // {
-    //   text: 'Career',
-    //   href: getPermalink('https://permias.karirlab.co/')
+    //   text: 'Blog',
+    //   links: [
+    //     {
+    //       text: 'Blog List',
+    //       href: getBlogPermalink(),
+    //     },
+    //     {
+    //       text: 'Article',
+    //       href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+    //     },
+    //     {
+    //       text: 'Article (with MDX)',
+    //       href: getPermalink('markdown-elements-demo-post', 'post'),
+    //     },
+    //     {
+    //       text: 'Category Page',
+    //       href: getPermalink('tutorials', 'category'),
+    //     },
+    //     {
+    //       text: 'Tag Page',
+    //       href: getPermalink('astro', 'tag'),
+    //     },
+    //   ],
     // },
   ],
 };
