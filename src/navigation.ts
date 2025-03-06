d2cd067cd1a8d5102c628961f49e725fad477968
fercananything <ferdinand.1238073@gmail.com>
2025-03-05 22:20:16 -0600

    Update English ver.

--- src/navigation.ts
+++ src/navigation.ts
@@ -98,7 +98,7 @@ export const footerData = {
     },
     {
       links: [
-        { text: 'Sejarah Kami', href: '/homes/sejarahKami' },
+        { text: 'Sejarah Kami', href: '/sejarah' },
         { text: 'Our Team', href: '/about/team' },
         { text: 'Chapters', href: '/about/chapters' },
         { text: 'Contact Us', href: '/contact' },
